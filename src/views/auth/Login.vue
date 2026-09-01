3bc20b4be994c13e06eefe84594751b9f6888c1b
YiFanAWA <202412214801011@zcmu.edu.cn>
2026-09-01 13:24:32 +0800

    fix: rename Windows-incompatible asset files

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -3,7 +3,7 @@
     <div class="login-container">
       <div class="header-container">
         <div class="header-icon">
-          <img src="/Snippet-64*64.svg" alt="Snippet logo" />
+          <img src="/Snippet-64x64.svg" alt="Snippet logo" />
         </div>
         <div class="header-title">
           <h1>欢迎来到 Snippet</h1>
